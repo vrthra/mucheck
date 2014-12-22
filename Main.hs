7de47a796605a686ff06f67778fcac4a8b37b998
Rahul Gopinath <gopinath@eecs.oregonstate.edu>
2014-12-22 15:07:39 -0800

    cleanup Main.hs

--- Main.hs
+++ Main.hs
@@ -14,44 +14,9 @@ extraOps = Var . UnQual . Ident <$> ["qsort", "id", "reverse"]
 
 
 -- NOTE: On QuickCheck.hs's mutants, it takes ~3-4 minutes to test 323 mutants on three properies.
-mytest = checkPropsOnMutants (take 13 $ genFileNames "Examples/Quicksort.hs") "Examples.Quicksort" ["quickCheckResult idEmpProp",  "quickCheckResult revProp", "quickCheckResult modelProp"] "./logs.txt"
+mytest = checkPropsOnMutants (take 13 mutantFiles) topModule  codes logFile
+  where mutantFiles = genFileNames "Examples/Quicksort.hs"
+        topModule = "Examples.Quicksort"
+        codes = ["quickCheckResult idEmpProp",  "quickCheckResult revProp", "quickCheckResult modelProp"]
+        logFile = "./logs.txt"
 
-
-
-{- Sample results:
-
-OVERALL RESULTS:
-
-Total number of mutants: 323
-Total number of alive and error-free mutants: 1 (0%)
-Total number of erroneous mutants: 0 (0%)
-
-=======================
-quickCheckResult idEmpProp
-
-Total number of mutants: 323
-
-Errors: 0 (0%)
-Successes (not killed): 30 (9%)
-Failures (killed): 293 (90%)
-Gaveups: 0 (0%)
-=======================
-quickCheckResult revProp
-
-Total number of mutants: 323
-
-Errors: 0 (0%)
-Successes (not killed): 7 (2%)
-Failures (killed): 316 (97%)
-Gaveups: 0 (0%)
-=======================
-quickCheckResult modelProp
-
-Total number of mutants: 323
-
-Errors: 0 (0%)
-Successes (not killed): 1 (0%)
-Failures (killed): 322 (99%)
-Gaveups: 0 (0%)
-
--}
