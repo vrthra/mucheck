fcbd98db6bebdca8a34c4a4b457e38a3c8c305f5
Rahul Gopinath <gopinath@eecs.oregonstate.edu>
2014-12-23 00:54:13 -0800

    spaces

--- MuCheck/Interpreter.hs
+++ MuCheck/Interpreter.hs
@@ -24,20 +24,19 @@ checkTestSuiteOnMutants = mutantCheckSummary
 
 -- main entry point
 mutantCheckSummary mutantFiles topModule evalSrcLst logFile  = do
-        results <- sequence $ map (runCodeOnMutants mutantFiles topModule) evalSrcLst
-        let singleTestSummaries = map (singleSummary mutantFiles) results
-            terminalSummary = fst $ multipleSummary results
-            logSummary = snd $ multipleSummary results
-            evalSrcLst' = zipWith (++) (repeat "\n=======================\n") evalSrcLst
-        -- print results to terminal
-        putStrLn $ "\n\n[]======== OVERALL RESULTS ========[]\n" ++ terminalSummary
-        putStrLn $ Mu.printStringList (zipWith (++) evalSrcLst' $ map fst singleTestSummaries)
-        -- print results to logfile
-        appendFile logFile $ "OVERALL RESULTS:\n" ++ logSummary ++ Mu.printStringList (zipWith (++) evalSrcLst' $ map snd singleTestSummaries)
-        putStr "\n[]===== END OF OVERALL RESULTS ====="
-        -- hacky solution to avoid printing entire results to stdout and to give
-        -- guidance to the type checker in picking specific Summarizable instances
-        return $ tail [head . map snd . snd . partitionEithers . head $ results]
+  results <- sequence $ map (runCodeOnMutants mutantFiles topModule) evalSrcLst
+  let singleTestSummaries = map (singleSummary mutantFiles) results
+      (terminalSummary,logSummary) = multipleSummary results
+      evalSrcLst' = zipWith (++) (repeat "\n=======================\n") evalSrcLst
+  -- print results to terminal
+  putStrLn $ "\n\n[]======== OVERALL RESULTS ========[]\n" ++ terminalSummary
+  putStrLn $ Mu.printStringList (zipWith (++) evalSrcLst' $ map fst singleTestSummaries)
+  -- print results to logfile
+  appendFile logFile $ "OVERALL RESULTS:\n" ++ logSummary ++ Mu.printStringList (zipWith (++) evalSrcLst' $ map snd singleTestSummaries)
+  putStr "\n[]===== END OF OVERALL RESULTS ====="
+  -- hacky solution to avoid printing entire results to stdout and to give
+  -- guidance to the type checker in picking specific Summarizable instances
+  return $ tail [head . map snd . snd . partitionEithers . head $ results]
 
 -- Interpreter Functionalities
 -- Examples
@@ -49,11 +48,11 @@ runCodeOnMutants mutantFiles topModule evalStr = sequence $ map (evalMyStr evalS
 -- result as a pair.
 evalMethod :: (I.MonadInterpreter m, Typeable t) => String -> String -> String -> m (String, t)
 evalMethod fileName topModule evalStr = do
-                I.loadModules [fileName]
-                I.setTopLevelModules [topModule]
-                I.setImports ["Prelude", "Test.QuickCheck", "Test.HUnit"]
-                result <- I.interpret evalStr (I.as :: (Typeable a => IO a)) >>= liftIO
-                return (fileName, result)
+  I.loadModules [fileName]
+  I.setTopLevelModules [topModule]
+  I.setImports ["Prelude", "Test.QuickCheck", "Test.HUnit"]
+  result <- I.interpret evalStr (I.as :: (Typeable a => IO a)) >>= liftIO
+  return (fileName, result)
 
 
 -- Class/Instance declaration
@@ -92,45 +91,47 @@ instance Summarizable HUnit.Counts where
 
 instance Summarizable Qc.Result where
     singleSummary mutantFiles results = (terminalMsg, logMsg)
-                  where (errorCases, executedCases) = partitionEithers results
-                        [successCases, failureCases, gaveUpCases] = map (\c -> filter (c . snd) executedCases) [Qc.isSuccess, isFailure, isGaveUp]
-                        r = length results
-                        e = length errorCases
-                        [s,f,g] = map length [successCases, failureCases, gaveUpCases]
-                        errorFiles = mutantFiles \\ map fst executedCases
-                        terminalMsg = "\n\nTotal number of mutants: " ++ show r ++
-                                      "\n\nErrors: " ++ show e ++ Mu.showPerCent (e `Mu.percent` r) ++
-                                      "\nSuccesses (not killed): " ++ show s ++ Mu.showPerCent (s `Mu.percent` r) ++
-                                      "\nFailures (killed): " ++ show f ++ Mu.showPerCent (f `Mu.percent` r) ++
-                                      "\nGaveups: " ++ show g ++ Mu.showPerCent (g `Mu.percent` r)
-                        logMsg = terminalMsg ++ "\n\nDetails:\n\nLoading error files:\n" ++ Mu.printlnList errorFiles
-                                ++ "\n\nLoading error messages:\n " ++ Mu.printlnList errorCases
-                                ++ "\n\nSUCCESSES:\n " ++ Mu.printlnList successCases
-                                ++ "\n\nFAILURE:\n " ++ Mu.printlnList failureCases
-                                ++ "\n\nGAVEUPs:\n " ++ Mu.printlnList gaveUpCases ++ "\n"
-                        isFailure :: Qc.Result -> Bool
-                        isFailure Qc.Failure{} = True
-                        isFailure _         = False
-                        isGaveUp :: Qc.Result -> Bool
-                        isGaveUp Qc.GaveUp{} = True
-                        isGaveUp _        = False
+      where (errorCases, executedCases) = partitionEithers results
+            [successCases, failureCases, gaveUpCases] = map (\c -> filter (c . snd) executedCases) [Qc.isSuccess, isFailure, isGaveUp]
+            r = length results
+            e = length errorCases
+            [s,f,g] = map length [successCases, failureCases, gaveUpCases]
+            errorFiles = mutantFiles \\ map fst executedCases
+            terminalMsg = "\n\nTotal number of mutants: " ++ show r ++
+                          "\n\nErrors: " ++ show e ++ Mu.showPerCent (e `Mu.percent` r) ++
+                          "\nSuccesses (not killed): " ++ show s ++ Mu.showPerCent (s `Mu.percent` r) ++
+                          "\nFailures (killed): " ++ show f ++ Mu.showPerCent (f `Mu.percent` r) ++
+                          "\nGaveups: " ++ show g ++ Mu.showPerCent (g `Mu.percent` r)
+            logMsg = terminalMsg ++ "\n\nDetails:\n\nLoading error files:\n" ++ Mu.printlnList errorFiles
+                    ++ "\n\nLoading error messages:\n " ++ Mu.printlnList errorCases
+                    ++ "\n\nSUCCESSES:\n " ++ Mu.printlnList successCases
+                    ++ "\n\nFAILURE:\n " ++ Mu.printlnList failureCases
+                    ++ "\n\nGAVEUPs:\n " ++ Mu.printlnList gaveUpCases ++ "\n"
+            isFailure :: Qc.Result -> Bool
+            isFailure Qc.Failure{} = True
+            isFailure _         = False
+            isGaveUp :: Qc.Result -> Bool
+            isGaveUp Qc.GaveUp{} = True
+            isGaveUp _        = False
 
     multipleSummary = multipleCheckSummary Qc.isSuccess
 
 -- we assume that checking each prop results in the same number of errorCases and executedCases
 multipleCheckSummary :: Show a => (a -> Bool) -> [[InterpreterOutput a]] -> (String, String)
 multipleCheckSummary isSuccessFunction results
-    | (not . and) (map ((==countMutants) . length) results ++ map ((==countExecutedCases) . length) executedCases) = error "Output lengths differ for some properties."
-    | otherwise = (terminalMsg, logMsg)
+  | not (checkLength results) = error "Output lengths differ for some properties."
+  | otherwise = (terminalMsg, logMsg)
    where executedCases = groupBy (\x y -> fst x == fst y) . sortBy (\x y -> fst x `compare` fst y) . rights $ concat results
          allSuccesses = [rs | rs <- executedCases, length rs == length results, and (map (isSuccessFunction . snd) rs)]
          countAlive = length allSuccesses
-         countMutants = length . head $ results
-         countExecutedCases = length . head $ executedCases
          countErrors = countMutants - length executedCases
          terminalMsg = "\nTotal number of mutants: " ++ show countMutants
                     ++ "\nTotal number of alive and error-free mutants: " ++ show countAlive
                     ++ Mu.showPerCent (countAlive `Mu.percent` countMutants) ++ "\n"
                     ++ "Total number of erroneous mutants (failed to be loaded): " ++ show countErrors ++ Mu.showPerCent (countErrors `Mu.percent` countMutants) ++ "\n"
          logMsg = terminalMsg ++ "\nDetails:\n\n" ++ Mu.printlnList allSuccesses ++ "\n"
+
+         checkLength results = and $ map ((==countMutants) . length) results ++ map ((==countExecutedCases) . length) executedCases
+         countExecutedCases = length . head $ executedCases
+         countMutants = length . head $ results
 
