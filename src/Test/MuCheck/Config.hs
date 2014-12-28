cf8edcd37d94c68780469909bab491a157ff1f8d
Rahul Gopinath <gopinath@eecs.oregonstate.edu>
2014-12-27 21:56:51 -0800

    add documentation for configuration options

--- src/Test/MuCheck/Config.hs
+++ src/Test/MuCheck/Config.hs
@@ -1,3 +1,4 @@
+-- | Configuration module
 module Test.MuCheck.Config where
 
 import Test.MuCheck.MuOp
@@ -8,15 +9,53 @@ data GenerationMode
   | FirstAndHigherOrder
   deriving (Eq, Show)
 
+-- | The configuration options
+-- if 1 is provided, all mutants are selected for that kind, and 0 ensures that
+-- no mutants are picked for that kind. Any fraction in between causes that
+-- many mutants to be picked randomly from the available pool
+
 data Config = Config { muOps :: [MuOp]
+-- | Mutate pattern matches for functions?
+-- for example
+--
+-- > first [] = Nothing
+-- > first (x:_) = Just x
+--
+-- is mutated to
+--
+-- > first (x:_) = Just x
+-- > first [] = Nothing
   , doMutatePatternMatches :: Rational
+-- | Mutates integer values by +1 or -1 or by replacing it with 0 or 1
   , doMutateValues :: Rational
+-- | negate if conditions, that is
+--
+-- > if True then 1 else 0
+--
+-- becomes
+--
+-- > if (not True) then 1 else 0
   , doNegateIfElse :: Rational
+-- | negate guarded booleans in case statements, that is
+--
+-- > case v of
+-- >   True -> 1
+-- >   otherwise -> 0
+--
+-- becomes
+--
+-- > case v of
+-- >  (not True) -> 1
+-- >  otherwise -> 0
   , doNegateGuards :: Rational
+-- | Maximum number of mutants to generate.
   , maxNumMutants :: Int
+-- | Generation mode, can be traditional (firstOrder) and
+-- higher order (higher order is experimental)
   , genMode :: GenerationMode }
   deriving Show
 
+-- | The default configuration
 defaultConfig :: Config
 defaultConfig = Config {muOps = allOps
   , doMutatePatternMatches = 1.0
