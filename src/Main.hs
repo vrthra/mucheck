67d9a4a5a42a87fced16866d7b5376380a872fe6
Rahul Gopinath <gopinath@eecs.oregonstate.edu>
2015-01-07 23:25:24 -0800

    TEMP: To be remvoed : add quick check instances

---
+++ src/Main.hs
@@ -0,0 +1,20 @@
+module Main where
+import System.Environment (getArgs)
+
+import Test.MuCheck (mucheck)
+import Test.MuCheck.TestAdapter.QuickCheck
+import Test.MuCheck.TestAdapter
+import Test.MuCheck.Utils.Print
+
+main :: IO ()
+main = do
+  val <- getArgs
+  case val of
+    ("-h" : _ ) -> help
+    (file : _args) -> do (msum, _tsum) <- mucheck (toRun file :: QuickCheckRun)
+                         putStrLn (show msum)
+    _ -> error "Need function file [args]\n\tUse -h to get help"
+
+help :: IO ()
+help = putStrLn $ "mucheck function file [args]\n" ++ showAS ["E.g:",
+       " mucheck-quickcheck Examples/QuickCheckTest.hs 'quickCheckResult idEmpProp' 'quickCheckResult revProp' 'quickCheckResult modelProp'",""]
---
+++ src/Test/MuCheck/TestAdapter/QuickCheck.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE StandaloneDeriving, DeriveDataTypeable, TypeSynonymInstances, MultiParamTypeClasses #-}
+-- | Module for using quickcheck properties
+module Test.MuCheck.TestAdapter.QuickCheck where
+import qualified Test.QuickCheck.Test as Qc
+import Test.MuCheck.TestAdapter
+
+import Data.Typeable
+
+deriving instance Typeable Qc.Result
+type QuickCheckSummary = Qc.Result
+
+-- | Summarizable instance of `QuickCheck.Result`
+instance Summarizable QuickCheckSummary where
+  testSummary _mutant _test result = Summary $ _ioLog result
+  isSuccess = Qc.isSuccess
+  isFailure Qc.Failure{} = True
+  isFailure _            = False
+  isOther   Qc.GaveUp{}  = True
+  isOther   _            = False
+
+
+data QuickCheckRun = QuickCheckRun String
+
+instance TRun QuickCheckRun QuickCheckSummary where
+  genTest _m tstfn = "quickCheckResult " ++ tstfn
+  getName (QuickCheckRun str) = str 
+  toRun s = QuickCheckRun s
+
+  summarize_ _m = (testSummary :: Mutant -> TestStr -> InterpreterOutput QuickCheckSummary -> Summary)
+  success_ _m = (isSuccess :: QuickCheckSummary -> Bool)
+  failure_ _m = (isFailure :: QuickCheckSummary -> Bool)
+  other_ _m = (isOther :: QuickCheckSummary -> Bool)
+
