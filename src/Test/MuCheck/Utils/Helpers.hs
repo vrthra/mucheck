158c164a2b0a4b123db6be406d525719aa8085d7
Caagr98 <caagr98@gmail.com>
2019-02-08 14:57:02 +0100

    Update to more modern stuff

--- src/Test/MuCheck/Utils/Helpers.hs
+++ src/Test/MuCheck/Utils/Helpers.hs
@@ -1,6 +1,6 @@
 -- | Helper module for easier visualization
 module Test.MuCheck.Utils.Helpers where
-import Language.Haskell.Exts.Annotated
+import Language.Haskell.Exts
 import Test.MuCheck.MuOp
 
 -- | Class to allow easier visualization of values without munging `show`
@@ -8,8 +8,8 @@ class Showx a where
   showx :: a -> String
 
 -- | Temporary holder for easier visualization
-data X = ModuleX (Module_)
-       | DeclX (Decl_)
+data X = ModuleX Module_
+       | DeclX Decl_
        | DeclXs [Decl_]
 
 -- | showx instances
